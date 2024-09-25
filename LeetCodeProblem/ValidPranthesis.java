beaeb4246191fce8c306f199a4d90bfc6b290728
Deepak <deepakappu961@gmail.com>
2024-09-25 16:43:34 +0530

    20

---
+++ LeetCodeProblem/ValidPranthesis.java
@@ -0,0 +1,10 @@
+public class ValidPranthesis {
+    public static void main(String[] args) {
+        String s= "()[]{}";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        return true;
+    }
+}
