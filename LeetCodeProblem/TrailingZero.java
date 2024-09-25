111d71d69d9ef28be33e9831496388d7795ef719
Deepak <deepakappu961@gmail.com>
2024-09-25 15:46:23 +0530

    172

---
+++ LeetCodeProblem/TrailingZero.java
@@ -0,0 +1,11 @@
+public class TrailingZero {
+    
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(trailingZeroes(n));
+    }
+
+    public static int trailingZeroes(int n) {
+        return 1;
+    }
+}
