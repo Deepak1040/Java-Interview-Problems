a5a8de34769282139bd00fc2ecfdc1b7e926b9bb
Deepak <deepakappu961@gmail.com>
2024-09-25 16:03:35 +0530

    172

--- LeetCodeProblem/TrailingZero.java
+++ LeetCodeProblem/TrailingZero.java
@@ -1,11 +1,28 @@
+
+//172 Factorial Trailing Zero
+
 public class TrailingZero {
     
     public static void main(String[] args) {
-        int n=5;
+        int n=3;
         System.out.println(trailingZeroes(n));
     }
 
     public static int trailingZeroes(int n) {
-        return 1;
+        int fact =factorial(n);
+        int num,count=0;
+
+        while(fact>0) {
+            num=fact%10;
+            if(num==0) count++;
+            fact=fact%10;
+        }
+        return count;
     }
+    private static int factorial(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return n*factorial(n-1);
+    }
+
 }
