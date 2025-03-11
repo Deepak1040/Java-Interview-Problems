d27fa1f801e154a471f2cc25e5bcb33b2de284f9
Deepak <deepakappu961@gmail.com>
2025-03-11 12:45:02 +0530

    add MonotonicArray class and comment out unused code in fibbonacciSeries

---
+++ LeetCodeProblem/MonotonicArray.java
@@ -0,0 +1,6 @@
+// 896 Monotonic Array
+public class MonotonicArray {
+    public static void main(String[] args) {
+        
+    }
+}
--- bridgelabs/fibbonacciSeries.java
+++ bridgelabs/fibbonacciSeries.java
@@ -1,90 +1,83 @@
 package bridgelabs;
 
-import java.util.Scanner;
-
-
-
 public class fibbonacciSeries {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        /* System.out.println("Enter The number:");
-    
-        int size=sc.nextInt();
-        int array[] = new int[size];
-        int first=0,second=1;
-        array[0]=0;
-        array[1]=1;
-        int third=first+second;
-        int index=2;
-        while(third<size){
-            array[index]=third;
-            first=second;
-            second=third;
-            third=first+second;
-            index++;
-        }
-        System.out.println(Arrays.toString(array));
-        sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter The number:");
 
-        String s= "Programming";
-        StringBuilder sb = new StringBuilder(s);
-        System.out.println(sb.reverse());
+        // int size = sc.nextInt();
+        // int array[] = new int[size];
+        // int first = 0, second = 1;
+        // array[0] = 0;
+        // array[1] = 1;
+        // int third = first + second;
+        // int index = 2;
+        // while (third < size) {
+        // array[index] = third;
+        // first = second;
+        // second = third;
+        // third = first + second;
+        // index++;
+        // }
+        // System.out.println(Arrays.toString(array));
+        // sc.close();
 
-        System.out.println();
+        // String s = "Programming";
+        // StringBuilder sb = new StringBuilder(s);
+        // System.out.println(sb.reverse());
 
-        for(int i=s.length()-1;i>=0;i--){
-            System.out.print(s.charAt(i));
-        }
+        // System.out.println();
 
-        String str ="1011";
-        System.out.println(Integer.parseInt(str,2));
+        // for (int i = s.length() - 1; i >= 0; i--) {
+        // System.out.print(s.charAt(i));
+        // }
 
-        for(int i= 2 ;i<size;i++){
-            while(size%i==0){
-                System.out.println(i+" ");
-                size=size/i;
-            }
-        }
-        if(size>2) System.out.println(size);
+        // String str = "1011";
+        // System.out.println(Integer.parseInt(str, 2));
 
+        // for (int i = 2; i < size; i++) {
+        // while (size % i == 0) {
+        // System.out.println(i + " ");
+        // size = size / i;
+        // }
+        // }
+        // if (size > 2)
+        // System.out.println(size);
 
-        LinkedList <Integer> l = new LinkedList<>(); 
-        l.add(10);
+        // LinkedList<Integer> l = new LinkedList<>();
+        // l.add(10);
 
-        System.out.println(l.toString());
+        // System.out.println(l.toString());
 
-        int arr1[]={1,2,3,4,5};
-        int arr2[]={3,5,7,9};
-        ArrayList<Integer> list= new ArrayList<>();
-        for(int i=0;i<arr1.length;i++){
-            for(int j=0;j<arr2.length;j++){
-                if(arr1[i]==arr2[j]){
-                    list.add(arr2[j]);
-                }
-            }
-        }
+        // int arr1[] = { 1, 2, 3, 4, 5 };
+        // int arr2[] = { 3, 5, 7, 9 };
+        // ArrayList<Integer> list = new ArrayList<>();
+        // for (int i = 0; i < arr1.length; i++) {
+        // for (int j = 0; j < arr2.length; j++) {
+        // if (arr1[i] == arr2[j]) {
+        // list.add(arr2[j]);
+        // }
+        // }
+        // }
+
+        // System.out.println(list.toString());
+
+        // String str = "the quick brown fox jumps over the lazy dog";
+
+        // HashMap<String, Integer> map = new HashMap<>();
+
+        // String array[] = str.split(" ");
+        // for (String s : array) {
+        // map.put(s, map.getOrDefault(s, 0) + 1);
+        // }
+
+        // for (Entry<String, Integer> e : map.entrySet()) {
+        // System.out.println(e.getKey() + " " + e.getValue());
+        // }
+
 
         
-        System.out.println(list.toString());
-
-
-        
-
-        String str= "the quick brown fox jumps over the lazy dog";
-
-        HashMap<String,Integer> map = new HashMap<>();
-
-        String array[]= str.split(" ");
-        for (String s : array) {
-            map.put(s,map.getOrDefault(s, 0)+1);
-        }
-
-        for(Entry<String,Integer> e : map.entrySet()){
-            System.out.println(e.getKey()+" "+e.getValue());
-        }
-
-        */
 
     }
 }
